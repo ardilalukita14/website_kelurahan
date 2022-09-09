4facc63d20d15d26faf8543dafc456fa1fe92db2
ardilalukita14 <lukitasari623@gmail.com>
2022-09-09 20:39:11 +0700

    develop website kelurahan

--- app/Http/Controllers/BeritaMainController.php
+++ app/Http/Controllers/BeritaMainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Berita;
+use App\Models\Content;
 use Session;
 
 
@@ -128,5 +129,85 @@ class BeritaMainController extends Controller
                     ->get();
         return view('berita/karta',compact('karta'));
 }
-        
+
+    public function kk() { 
+        $kk =  Content::orderBy('created_at','DESC')
+                    ->where('id_layanan',2)
+                    ->take(1)
+                    ->get();
+        return view('pelayanan/kk',compact('kk'));
+}
+
+    public function ktp() { 
+        $ktp =  Content::orderBy('created_at','DESC')
+                    ->where('id_layanan',3)
+                    ->take(1)
+                    ->get();
+        return view('pelayanan/ktp',compact('ktp'));
+}
+
+    public function dokkel() { 
+        $dokkel =  Content::orderBy('created_at','DESC')
+                    ->where('id_layanan',4)
+                    ->take(1)
+                    ->get();
+        return view('pelayanan/dokumenkelahiran',compact('dokkel'));
+}
+
+    public function pp() { 
+        $pp =  Content::orderBy('created_at','DESC')
+                    ->where('id_layanan',5)
+                    ->take(1)
+                    ->get();
+        return view('pelayanan/pengantarpindah',compact('pp'));
+}
+
+    public function ppd() { 
+        $ppd =  Content::orderBy('created_at','DESC')
+                    ->where('id_layanan',6)
+                    ->take(1)
+                    ->get();
+        return view('pelayanan/pindahdatang',compact('ppd'));
+}
+
+    public function km() { 
+        $km =  Content::orderBy('created_at','DESC')
+                    ->where('id_layanan',7)
+                    ->take(1)
+                    ->get();
+        return view('pelayanan/keteranganmenikah',compact('km'));
+}
+
+    public function kaw() { 
+        $kaw =  Content::orderBy('created_at','DESC')
+                    ->where('id_layanan',8)
+                    ->take(1)
+                    ->get();
+        return view('pelayanan/ahliwaris',compact('kaw'));
+}
+
+    public function dokem() { 
+        $dokem =  Content::orderBy('created_at','DESC')
+                    ->where('id_layanan',9)
+                    ->take(1)
+                    ->get();
+        return view('pelayanan/dokumenkematian',compact('dokem'));
+}
+
+    public function ktm() { 
+        $ktm =  Content::orderBy('created_at','DESC')
+                    ->where('id_layanan',10)
+                    ->take(1)
+                    ->get();
+        return view('pelayanan/tidakmampu',compact('ktm'));
+    }
+
+
+    public function skck() { 
+        $skck =  Content::orderBy('created_at','DESC')
+                    ->where('id_layanan',11)
+                    ->take(1)
+                    ->get();
+        return view('pelayanan/skck',compact('skck'));
+}
 }
